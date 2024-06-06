fc59c7bf61130df0001af35c4138167ac98f6dbb
Marcus Rådell <marcus@radell.net>
2024-06-06 21:05:53 +0200

    set basic structure for top nav

--- src/components/top-nav/top-nav.tsx
+++ src/components/top-nav/top-nav.tsx
@@ -1,24 +1,34 @@
-const Brand = () => "Monadium";
+const Brand = () => (
+  <div className="navbar-start">
+    <a className="btn btn-ghost text-xl" href="/">
+      Monadium
+    </a>
+  </div>
+);
 
-const HomeLink = () => "Home";
+type Props = { text: string; href: string };
+
+const NavLink = ({ text, href }: Props) => (
+  <li>
+    <a href={href}>{text}</a>
+  </li>
+);
+
+const Home = () => "Home";
 
 const Media = () => "Media";
 
 const Contact = () => "Contact";
 
 export const TopNav = () => (
-  <nav className="flex">
+  <nav className="navbar bg-base-100">
     <Brand />
-    <ul className="flex">
-      <li>
-        <HomeLink />
-      </li>
-      <li>
-        <Media />
-      </li>
-      <li>
-        <Contact />
-      </li>
-    </ul>
+    <div className="navbar-center">
+      <ul className="menu menu-horizontal">
+        <NavLink href="/" text="Home" />
+        <NavLink href="/media" text="Media" />
+        <NavLink href="/contact" text="Contact" />
+      </ul>
+    </div>
   </nav>
 );
