909ac00f19814b473a47358297b9ec24aab55837
Marcus Rådell <marcus@radell.net>
2024-06-06 20:43:39 +0200

    add topnav skeleton

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,28 @@
+const Brand = () => "Monadium";
+
+const HomeLink = () => "Home";
+
+const Media = () => "Media";
+
+const Contact = () => "Contact";
+
 export default function Home() {
   return (
     <main className="">
-      <nav>top nav</nav>
+      <nav className="flex">
+        <Brand />
+        <ul className="flex">
+          <li>
+            <HomeLink />
+          </li>
+          <li>
+            <Media />
+          </li>
+          <li>
+            <Contact />
+          </li>
+        </ul>
+      </nav>
       <div className="container">container</div>
       <footer>footer</footer>
     </main>
