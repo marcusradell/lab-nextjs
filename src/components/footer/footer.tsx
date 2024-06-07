f83e553cac6afe2883688f1ae9dc8370f408e63a
Marcus Rådell <marcus@radell.net>
2024-06-07 19:00:19 +0200

    set copyfree footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,3 +1,27 @@
 export function Footer() {
-  return "footer";
+  return (
+    <footer className="footer p-10 bg-neutral text-neutral-content">
+      <nav>
+        <h4 className="footer-title">Pages</h4>
+        <a href="/" className="link link-hover">
+          Home
+        </a>
+        <a href="/media" className="link link-hover">
+          Media
+        </a>
+        <a href="/contact" className="link link-hover">
+          Contact
+        </a>
+      </nav>
+
+      <nav>
+        <h4 className="footer-title">Copyright</h4>
+        <span>
+          I waive all copyright interests and place this content in the public
+          domain, making it free for use by anyone for any purpose.
+        </span>
+        <span>Copyfree Marcus Rådell 2024</span>
+      </nav>
+    </footer>
+  );
 }
